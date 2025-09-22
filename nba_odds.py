7f92fbf42322ca5af5f3da859e0d41102b3ad877
jonvabalis <tmotiejus@gmail.com>
2025-09-22 23:45:19 +0300

    tasks 3/10

--- nba_odds.py
+++ nba_odds.py
@@ -23,6 +23,41 @@ def american_to_european(odds):
         return 1 + (100 / abs(odds))
 
 
+def print_continuous_attribute_data(df, attribute):
+    rowCount = len(df[attribute])
+    rowCountMissing = df[attribute].isnull().sum()
+    missingPercentage = (rowCountMissing / rowCount) * 100
+
+    print(f"-----{attribute}-----")
+    print(f"Row count: {len(df[attribute]) - df[attribute].isnull().sum()}")
+    print(f"Missing data row percentage: {missingPercentage:.2f}%")
+    print(f"Cardinality: {df[attribute].nunique()}")
+    print(f"Min value: {df[attribute].min()}")
+    print(f"Max value: {df[attribute].max()}")
+    print(f"1st quantile: {df[attribute].quantile(0.25):.2f}")
+    print(f"3rd quantile: {df[attribute].quantile(0.75):.2f}")
+    print(f"Average: {df[attribute].mean():.2f}")
+    print(f"Median: {df[attribute].median():.2f}")
+    print(f"Standard deviation: {(df[attribute].std()):.2f}")
+
+
+def print_categorical_attribute_data(df, attribute):
+    rowCount = len(df[attribute])
+    rowCountMissing = df[attribute].isnull().sum()
+    missingPercentage = (rowCountMissing / rowCount) * 100
+
+    print(f"-----{attribute}-----")
+    print(f"Row count: {len(df[attribute]) - df[attribute].isnull().sum()}")
+    print(f"Missing data row percentage: {missingPercentage:.2f}%")
+    print(f"Cardinality: {df[attribute].nunique()}")
+    print(f"Mode: {df[attribute].value_counts().index[0]}")
+    print(f"Mode frequency value: {df[attribute].value_counts().iloc[0]}")
+    print(f"Mode frequency percentage: {((df[attribute].value_counts().iloc[0] / len(df[attribute])) * 100):.2f}%")
+    print(f"2nd mode: {df[attribute].value_counts().index[1]}")
+    print(f"2nd mode frequency value: {df[attribute].value_counts().iloc[1]}")
+    print(f"2nd mode frequency percentage: {((df[attribute].value_counts().iloc[1] / len(df[attribute])) * 100):.2f}%")
+
+
 df2019["moneyLine"] = df2019["moneyLine"].apply(american_to_european)
 df2019["opponentMoneyLine"] = df2019["opponentMoneyLine"].apply(american_to_european)
 
@@ -49,13 +84,27 @@ df2019 = df2019.drop(columns=['date', 'season', 'total', 'secondHalfTotal'])
 df2019.loc[10, "score"] = np.nan
 df2019.loc[100, "opponentScore"] = np.nan
 df2019.loc[1000, "team"] = np.nan
+df2019.loc[20, "team"] = np.nan
+df2019.loc[200, "home/visitor"] = np.nan
+df2019.loc[1200, "team"] = np.nan
+df2019.loc[1202, "team"] = np.nan
+df2019.loc[1203, "team"] = np.nan
+df2019.loc[1204, "team"] = np.nan
+df2019.loc[1205, "team"] = np.nan
 
-rowCount = len(df2019)
-rowCountMissing = df2019.isnull().any(axis=1).sum()
-missingPercentage = (rowCountMissing / rowCount) * 100
+# Continuous attributes
+print("Continuous attributes")
+continuous_attributes = ["score", "opponentScore", "moneyLine", "opponentMoneyLine", "spread"]
+for attribute in continuous_attributes:
+    print_continuous_attribute_data(df2019, attribute)
 
-print(f"Current row count: {rowCount}, missing {rowCountMissing} rows,"
-      f" thus missing data row percentage is {missingPercentage:.2f}%")
+print("")
+
+# Categorical attributes
+print("Categorical attributes")
+continuous_attributes = ["team", "home/visitor", "opponent"]
+for attribute in continuous_attributes:
+    print_categorical_attribute_data(df2019, attribute)
 
 # As I have many rows of data, I will drop the rows with missing data
 # Row count went 1230 -> 1227
